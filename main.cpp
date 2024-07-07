b552e4496ece1ae6f77b2299b79444963dc83c3b
pamiisphere <167950595+pamiisphere@users.noreply.github.com>
2024-07-07 21:27:32 +0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  First arg
+//
+//  Created by Mingmanas Sivaraksa on 2/1/2567 BE.
+//
+
+#include <iostream>
+using namespace std;
+#include <iomanip>
+#include "sorting.h"
+
+int main(int argc, char * argv[]) {
+    int i,N=argc-1;
+    int *a=new int[N];
+    for(i=1;i<argc;i++){
+        
+        a[i-1]=atoi(argv[i]);
+    
+         
+    }
+    bubbleSort(a,N);
+    delete []a;
+    return 0;
+}
